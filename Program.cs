43a1c56471f349e072bc636fcd9a397a1493bb64
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-03-26 21:28:47 -0300

    Corrigindo bugs na leitura do arquivo csv e criando o método DescobreBolsistaZeroDoAno

--- Program.cs
+++ Program.cs
@@ -13,16 +13,39 @@
                 if (contador >= 1)
                 {
                     var colunas = linha.Split(",");
-                    var nome = colunas[0];
-                    var nome2 = nome.Split();
-                    var cpf = colunas[1];
-                    var entidadeDeEnsino = colunas[2];
-                    var valorDaBolsa = double.Parse(colunas[10]);
-                    var anoQueRecebeu = int.Parse(colunas[4]);
-                    bolsistas.Add(new Bolsista(nome, cpf, entidadeDeEnsino, valorDaBolsa, anoQueRecebeu));
+                    var nome = colunas[0].Replace('/',' ');
+                    nome = nome.Replace('"', ' ');
+                    var cpf = colunas[1].Replace('/',' ');
+                    cpf = cpf.Replace('"', ' ');
+                    var entidadeDeEnsino = colunas[2].Replace('/',' ');
+                    entidadeDeEnsino = entidadeDeEnsino.Replace('"', ' ');
+                    var valorDaBolsa = colunas[10].Replace('/',' ');
+                    valorDaBolsa = valorDaBolsa.Replace('"', ' ');
+                    valorDaBolsa = valorDaBolsa.Replace(" ", "");
+                    var anoQueRecebeu = colunas[4].Replace('/',' ');
+                    anoQueRecebeu = anoQueRecebeu.Replace('"', ' ');
+                    anoQueRecebeu = anoQueRecebeu.Replace(" ", "");
+                    bolsistas.Add(new Bolsista(nome, cpf, entidadeDeEnsino, double.Parse(valorDaBolsa), int.Parse(anoQueRecebeu)));
                 }
                 contador++;
             }
         }
+        public static void DescobreBolsistaZeroDoAno(List<Bolsista> bolsistas){
+            Console.WriteLine("Deseja saber quem foi o bolsista zero de qual ano? Ex: 2015");
+            int anoEscolhido = int.Parse(Console.ReadLine());
+            var bolsistaZero = bolsistas.Find(zero => zero.AnoQueRecebeu == anoEscolhido);
+            Console.WriteLine($"O bolsista zero no ano de {anoEscolhido} é:");
+            Console.WriteLine($"{bolsistaZero.Nome}, CPF: {bolsistaZero.CPF}, para estudar na: {bolsistaZero.EntidadeDeEnsino} e com uma bolsa no valor de {bolsistaZero.ValorDaBolsa}");
+        }
+        public static void CriptografarNome(){
+
+        }
+        public static void ValorMedioDasBolsasNoAno(){
+
+        }
+        public static void RankearBolsasTresMaisETresMenos(){
+
+        }
+
     }
 }
